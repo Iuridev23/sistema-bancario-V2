804c913a065bca1256ecaa419a1dbfa5e7f6af5b
Iuridev <iurigames23@gmail.com>
2023-08-08 12:29:02 -0300

    Operação de Imprimir Extrato feita

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -6,32 +6,38 @@ def deposito(saldo, valor, extrato):
     else:
         print("Valor Inválido!")
 
-def saque(saldo=0, valor=0, extrato="", numero_saques=0, limite_saques=3):   
-        print("-----------Saque-----------")
+def saque(saldo=0, valor=0, extrato="", numero_saques=0, limite_saques=3):    
+    saques_excedidos = numero_saques >= limite_saques
+
+    if saques_excedidos:
+        print("Número diário de saques excedido!")
+        return saldo, extrato, numero_saques            
+    else:     
+        saldo_excedido = valor > saldo
+        limite_excedido = valor > 500
         
-        saques_excedidos = numero_saques >= limite_saques
+        if limite_excedido:            
+            print("Valor do saque excedeu o limite!")
+            return saldo, extrato, numero_saques
+        elif saldo_excedido:
+            print("Saldo Insuficiente!")
+            return saldo, extrato, numero_saques 
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: -R$ {valor:.2f} \nSaldo: R$ {saldo:.2f}\n"
+            numero_saques += 1
+            return saldo, extrato, numero_saques
+        else:
+            print("Valor Inválido!")
 
-        if saques_excedidos:
-            print("Número diário de saques excedido!")
-            return saldo, extrato, numero_saques            
-        else:     
-            saldo_excedido = valor > saldo
-            limite_excedido = valor > 500
-            
-            if limite_excedido:            
-                print("Valor do saque excedeu o limite!")
-                return saldo, extrato, numero_saques
-            elif saldo_excedido:
-                print("Saldo Insuficiente!")
-                return saldo, extrato, numero_saques 
-            elif valor > 0:
-                saldo -= valor
-                extrato += f"Saque: -R$ {valor:.2f} \nSaldo: R$ {saldo:.2f}\n"
-                numero_saques += 1
-                return saldo, extrato, numero_saques
-            else:
-                print("Valor Inválido!")
-
+def imprimirExtrato(saldo, extrato=""):
+    if not extrato:
+        print("Não foram realizadas movimentações.")
+        print(f"----------- \nSaldo Total: R$ {saldo:.2f}")
+    else:
+        print("-----------Extrato-----------")
+        print(extrato)
+        print(f"----------- \nSaldo Total: R$ {saldo:.2f}")
 
 menu = """
 Escolha uma opção:
@@ -63,16 +69,11 @@ while True:
         valores_saque = (saque(saldo, float(input("Qual o valor a ser sacado? ")), extrato, contator_saque))
         saldo = valores_saque[0]
         extrato = valores_saque[1]
-        contator_saque = valores_saque[2]
-        print(f"{saldo}, {extrato}, {contator_saque}") 
+        contator_saque = valores_saque[2]       
             
-#     elif opcao == "3":
-#         if not extrato:
-#             print("Não foram realizadas movimentações.")
-#         else:
-#             print("-----------Extrato-----------")
-#             print(extrato)
-#             print(f"----------- \nSaldo Total: R$ {saldo:.2f}")
+    elif opcao == "3":
+        imprimirExtrato(saldo, extrato=extrato)
+
 
     elif opcao == "0":
         print("Saindo da conta...")
