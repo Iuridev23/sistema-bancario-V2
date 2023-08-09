db608468766ed08726f48ed44b1768913e5af01a
Iuridev <iurigames23@gmail.com>
2023-08-08 21:40:47 -0300

    Projeto Finalizado

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -29,6 +29,7 @@ def saque(saldo=0, valor=0, extrato="", numero_saques=0, limite_saques=3):
             return saldo, extrato, numero_saques
         else:
             print("Valor Inválido!")
+            return saldo, extrato, numero_saques
 
 def imprimirExtrato(saldo, extrato=""):
     if not extrato:
@@ -39,20 +40,66 @@ def imprimirExtrato(saldo, extrato=""):
         print(extrato)
         print(f"----------- \nSaldo Total: R$ {saldo:.2f}")
 
+def criar_usuario(usuarios):
+    cpf = input("Informe o CPF (somente número): ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\nJá existe usuário com esse CPF!")
+        return
+
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe o endereço (logradouro, nro - bairro - cidade/sigla estado): ")
+
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+
+    print("=== Usuário criado com sucesso! ===")
+
+
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n=== Conta criada com sucesso! ===")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+
+    print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
+
+
+def listar_contas(contas):
+    for conta in contas:
+        linha = f"""\
+            Agência:\t{conta['agencia']}
+            C/C:\t\t{conta['numero_conta']}
+            Titular:\t{conta['usuario']['nome']}
+        """
+        print((linha))
+
 menu = """
 Escolha uma opção:
 1 - Depositar
 2 - Sacar
 3 - Imprimir Extrato
+4 - Criar Usuário
+5 - Criar Conta
+6 - Listar Conta
 0 - Sair
 
 """
 
 saldo = 0
-# LIMITE = 500
 extrato = ""
-# LIMITE_SAQUES = 3
 contator_saque = 0
+usuarios = []
+contas = []
+AGENCIA = "0001"
 
 while True:
     opcao = input(menu)
@@ -73,7 +120,19 @@ while True:
             
     elif opcao == "3":
         imprimirExtrato(saldo, extrato=extrato)
+    
+    elif opcao == "4":
+        criar_usuario(usuarios)
+    
+    elif opcao == "5":
+        numero_conta = len(contas) + 1
+        conta = criar_conta(AGENCIA, numero_conta, usuarios)
 
+        if conta:
+            contas.append(conta)
+    
+    elif opcao == "6":
+        listar_contas(contas)
 
     elif opcao == "0":
         print("Saindo da conta...")
